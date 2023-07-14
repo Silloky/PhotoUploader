186e9ec1314489c89abea171691fbcd61e896e8f
Silloky <ek.mindstorms@outlook.com>
2023-07-14 20:20:01 +0200

    Feat : main page structure & rough grid css

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ if (!(array_key_exists('connected',$_SESSION))){
     }
 }
 
+$rand = random_int(1,10000)
+
 
 ?>
 
@@ -23,31 +25,29 @@ if (!(array_key_exists('connected',$_SESSION))){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload Photos</title>
     <script src="./app.js" defer></script>
-    <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="./style.css?refreshthing=<?= $rand ?>">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
-    <main>
-        <div id="sidebar">
-            <div id="photos">
+    <div id="sidebar">
+        <div id="photos">
 
-            </div>
-            <div id="controls">
+        </div>
+        <div id="controls">
+
+        </div>
+    </div>
+    <div id="editor-panel">
+        <!-- <img src="" alt="preview"> -->
+        <div id="fields">
+
+            <div id="map">
 
             </div>
         </div>
-        <div id="editor-panel">
-            <img src="" alt="preview">
-            <div id="fields">
-
-                <div id="map">
-                    
-                </div>
-            </div>
-        </div>
-    </main>
+    </div>
     <div id="bottom-bar">
 
     </div>
