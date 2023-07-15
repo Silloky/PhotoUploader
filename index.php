52544b64cc94d61872cb70b92aa6950d1d59e09e
Silloky <ek.mindstorms@outlook.com>
2023-07-15 05:57:12 +0200

    Feat : posh controls

--- index.php
+++ index.php
@@ -26,18 +26,28 @@ $rand = random_int(1,10000)
     <title>Upload Photos</title>
     <script src="./app.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="./style.css?refreshthing=<?= $rand ?>">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
     <div id="sidebar">
         <div id="photos">
 
         </div>
-        <div id="controls">
-
-        </div>
+        <ul id="controls">
+            <li class="control">
+                <span class="material-symbols-rounded">add_photo_alternate</span>
+                <span>Add photo(s)</span>
+            </li>
+            <li class="control">
+                <span class="material-symbols-rounded">delete_forever</span>
+                <span>Remove selected photo(s)</span>
+            </li>
+            <li class="control">
+                <span class="material-symbols-rounded">help</span>
+                <span>Show help</span>
+            </li>
+        </ul>
     </div>
     <div id="editor-panel">
         <!-- <img src="" alt="preview"> -->
