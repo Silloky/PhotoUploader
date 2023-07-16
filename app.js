65685584ed2a854e0333d02564cf3db30fa1be80
Silloky <ek.mindstorms@outlook.com>
2023-07-16 05:10:02 +0200

    Feat : add photoblock on photo upload (JS)

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-function addPhotoBlock(){
-    
+function addPhotoBlock(file){
+    var url = URL.createObjectURL(file)
+    var date = file.lastModifiedDate.toLocaleDateString()
+    var time = file.lastModifiedDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+    var html = `
+    <div class="photoblock">
+            <img src="${url}" alt="">
+            <span>${file.name}</span>
+            <span>${date} ${time}</span>
+    </div>
+    `
+    photosBar.append(html)
+
 }
 
 // $(window).on('load',function() {
 //     $('.material-icons-rounded').css('display','inline');
 // });
 
+let photos = Array()
+var photosBar = $("#photos")
+
 $("#filebrowser")[0].onchange = evt => {
-    Array.from($("#filebrowser")[0].files).forEach(file => {
-        console.log(file.name)
+    var justImportedPhotos = Array.from($("#filebrowser")[0].files)
+    justImportedPhotos.forEach(file => {
+        photos.push(file)
+        addPhotoBlock(file)
     })
 }
