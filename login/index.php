d6cdb14e7fd5a67439c32805cf18f6a13e1b05e8
Silloky <ek.mindstorms@outlook.com>
2023-08-14 15:11:08 +0200

    Refactor : move files around

--- login/index.php
+++ login/index.php
@@ -57,10 +57,10 @@ $rand = random_int(1,10000);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../colors.css?refreshthing=<?= $rand ?>">
+    <link rel="stylesheet" href="../modules/colors.css?refreshthing=<?= $rand ?>">
     <link rel="stylesheet" href="./login.css?refreshthing=<?= $rand ?>">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <script src="./login.js?refreshthing=<?= $rand ?>" defer></script>
+    <script src="../login.js?refreshthing=<?= $rand ?>" defer></script>
     <script src="../lib/jquery.js"></script>
 </head>
 <body>
