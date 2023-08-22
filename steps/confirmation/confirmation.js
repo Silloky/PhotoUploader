c4504b94fb93f462b481cd12659fe9af8a8740c5
Silloky <ek.mindstorms@outlook.com>
2023-08-22 14:21:11 +0200

    Feat : review page

---
+++ steps/confirmation/confirmation.js
@@ -0,0 +1,42 @@
+const listDiv = $("#list")
+
+photos.forEach((photo, index) => {
+    var url = photo.data
+    var name = photo.name
+    var saveLocation = '/test/path/forthe/moment'
+    var date = photo.lastModifiedDate.toLocaleString('fr-FR')
+    var gpsLocation = photo.gpsLocation
+    if (gpsLocation == undefined){
+        gpsLocation = 'None'
+    }
+    var html = 
+    `<div class="photo">
+        <div class="photo-preview">
+            <img src="${url}" alt="">
+        </div>
+        <div class="photo-data">
+            <div>
+                <span>Name</span>
+                <span>${name}</span>
+            </div>
+            <div>
+                <span>Save Location</span>
+                <span>${saveLocation}</span>
+            </div>
+            <div>
+                <span>Date & Time</span>
+                <span>${date}</span>
+            </div>
+            <div>
+                <span>GPS Location</span>
+                <span>${gpsLocation}</span>
+            </div>
+        </div>
+        <span class="material-symbols-rounded" class="edit-btn">edit</span>
+    </div>`
+    listDiv.append(html)
+    if (index != photos.length - 1){
+        var html = '<div class="photo-spacer"></div>'
+        listDiv.append(html)
+    }
+})
--- steps/confirmation/confirmation.php
+++ steps/confirmation/confirmation.php
@@ -1,9 +1,19 @@
+<?php $relPath = substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT'])); ?>
 
+<link rel="stylesheet" href="<?= $relPath . "/confirmation.css" ?>">
+<script src="<?= $relPath . "/confirmation.js" ?>" defer></script>
 
-
+<div></div>
+<div id="reviewer-panel" class="scrollbarhidden">
+    <span class="big-text">Review</span>
+    <div id="list">
+        
+    </div>
+</div>
+<div></div>
 <div id="bottom-bar">
     <button type="button">Continue</button>
-    <button type="button">Go back</button>
+    <button type="button" onclick="changeStep('editing')">Go back</button>
 </div>
 <?php
     include('../../modules/toasts/toasts.php');
