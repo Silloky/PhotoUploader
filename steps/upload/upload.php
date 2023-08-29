8393c38da351b0ccad13ac99ea981494dd37877b
Silloky <ek.mindstorms@outlook.com>
2023-08-29 09:17:44 +0200

    Fix : remove bottom bar from upload page

--- steps/upload/upload.php
+++ steps/upload/upload.php
@@ -10,9 +10,6 @@
         
     </div>
 </div>
-<div id="bottom-bar">
-    <button type="button" onclick="tempBack()">Go back</button>
-</div>
 <?php
     include('../../modules/toasts/toasts.php');
 ?>
