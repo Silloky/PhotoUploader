e3e874e13c9b6fefa4f3bf249d763d722a9779e7
Silloky <ek.mindstorms@outlook.com>
2023-10-22 10:46:14 +0200

    Fix : add transitions as option for dev purposes

--- steps/loader.js
+++ steps/loader.js
@@ -1,36 +1,58 @@
 if (sessionStorage.getItem('step') == null){
     sessionStorage.setItem('step', 'editing')
 }
-$.get('./steps/editing/editing.php', function(data){
-    $("body").append($(data).hide())
-    setTimeout(function(){
-        $("body").children().not("link, script, #toast-hider").show()
-        $("#loader").fadeTo("slow", 0, function(){
-            $("#loader").hide()
-        })
-    },5000)
-})
 
-function changeStep(step){
-    sessionStorage.setItem('step', step)
-    $("body").find('*').not("#loader, .spinner").remove()
-    $("#loader").fadeTo('slow', 1, function(){
+const spinner = localStorage.getItem('spinner')
+
+if (spinner == 1){
+    $.get('./steps/editing/editing.php', function(data){
+        $("body").append($(data).hide())
+        setTimeout(function(){
+            $("body").children().not("link, script, #toast-hider").show()
+            $("#loader").fadeTo("slow", 0, function(){
+                $("#loader").hide()
+            })
+        },5000)
+    })
+    
+    function changeStep(step){
+        sessionStorage.setItem('step', step)
+        $("body").find('*').not("#loader, .spinner").remove()
+        $("#loader").fadeTo('slow', 1, function(){
+            var loadURL = './steps/' + step + '/' + step + '.php'
+            $.get(loadURL, function(data){
+                $("body").append($(data).hide())
+                setTimeout(function(){
+                    $("body").children().not("link, script, #toast-hider").show()
+                    $("#loader").fadeTo("slow", 0, function(){
+                        $("#loader").hide()
+                    })
+                }, 3000)
+            })
+        })
+    }
+
+    window.onbeforeunload = function(event){
+        if (logoutCount == 0){
+            return 'Are you sure you want to reload ?'
+        }
+    }
+
+} else {
+    $.get('./steps/editing/editing.php', function(data){
+        $("body").append($(data).hide())
+        $("body").children().not("link, script, #toast-hider").show()
+        $("#loader").hide()
+    })
+    
+    function changeStep(step){
+        sessionStorage.setItem('step', step)
         var loadURL = './steps/' + step + '/' + step + '.php'
         $.get(loadURL, function(data){
             $("body").append($(data).hide())
-            setTimeout(function(){
-                $("body").children().not("link, script, #toast-hider").show()
-                $("#loader").fadeTo("slow", 0, function(){
-                    $("#loader").hide()
-                })
-            }, 3000)
+            $("body").children().not("link, script, #toast-hider").show()
+            $("#loader").hide()
         })
-    })
-}
-
-window.onbeforeunload = function(event){
-    if (logoutCount == 0){
-        return 'Are you sure you want to reload ?'
     }
 }
 
